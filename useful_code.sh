02ef7415e996cbca95e72fe060cd3426f31518e6
Tom Stanton <tom@toms-mbp.mynet>
2021-12-29 14:56:21 +0000

    first commit

---
+++ useful_code.sh
@@ -0,0 +1,7 @@
+# A collection of useful sh commands
+
+## File/text-stream manipulation
+
+##In the general case where X is the number of initial lines to delete, credit to commenters and editors for this:
+
+sed 1,Xd file.txt
